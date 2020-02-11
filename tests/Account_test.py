28caaaf0e059429ce68cb4fbbeea8f7f5878d498
Robert <bibbycodes@gmail.com>
2020-02-11 16:59:04 +0000

    can initialize instance with account balance

--- tests/Account_test.py
+++ tests/Account_test.py
@@ -3,3 +3,7 @@ from lib.Account import Account
 def test_init_account_instance():
   account = Account()
   assert account.starting_balance == 0
+
+def test_init_account_instance_with_starting_balance():
+  account = Account(300)
+  assert account.starting_balance == 300
