a0f8c4d4a926e41dfba1956cd99cbde3be6a94a2
Robert <bibbycodes@gmail.com>
2020-02-11 17:15:32 +0000

    added edge cases for init account, can deposit

--- lib/Account.py
+++ lib/Account.py
@@ -1,3 +1,11 @@
 class Account:
   def __init__(self, starting_balance = 0):
-    self.starting_balance = starting_balance
+    if type((starting_balance)) in [int, float]:
+      self.starting_balance = starting_balance
+    else:
+      self.starting_balance = 0
+    self.balance = starting_balance
+
+  def deposit(self, amount = 0):
+    self.balance += amount
+    return amount
--- tests/Account_test.py
+++ tests/Account_test.py
@@ -7,3 +7,19 @@ def test_init_account_instance():
 def test_init_account_instance_with_starting_balance():
   account = Account(300)
   assert account.starting_balance == 300
+
+def test_init_account_with_string_as_balance():
+  account = Account("hello")
+  assert account.starting_balance == 0
+
+def test_init_account_with_none_value():
+  account = Account(None)
+  assert account.starting_balance == 0
+
+def test_deposit_with_zero_amount():
+  account = Account()
+  assert account.deposit(0) == 0
+
+def test_deposit_with_positive_amount():
+  account = Account()
+  assert account.deposit(100) == 100
